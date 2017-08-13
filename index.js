6291312e0a2611ea6a88014581c7a6bb08beaaaa
Yura Choi <yc1435@nyu.edu>
2017-08-13 03:12:19 +0000

    Done.

--- index.js
+++ index.js
@@ -11,6 +11,19 @@ const robots = [
   { name: 'Ratchet', alliance: null }
 ];
 
+
+
+const sortedRobots =
+robots.map(function (robot){
+  return Object.assign( {}, robot, {
+    name: robot.name,
+    alliance: (knownDecepticons.includes(robot.name) ? "decepticon" : "autobot")
+  });
+});
+
+
+
+
 const zebraStripes = [
   { width: 9.12, color: null },
   { width: 5.71, color: null },
@@ -21,3 +34,10 @@ const zebraStripes = [
   { width: 0.59, color: null },
   { width: 7.31, color: null }
 ];
+
+const coloredZebraStripes =
+zebraStripes.map(function(zebra, i) {
+  return Object.assign({}, zebra, {
+    color: (i % 2 === 0 ? "black" : "white")
+  });
+});
